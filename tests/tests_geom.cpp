ee6108bc9afdadf7e31a3b129fe684a52870ccad
Julien LE THENO <julien.letheno@gmail.com>
2020-05-28 16:36:07 +0200

    [Add] Add tests for geometry

---
+++ tests/tests_geom.cpp
@@ -0,0 +1,42 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <unistd.h>
+#include <opengl_framework.hpp>
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+Ogl_world world = Ogl_world(Color(0,0,0));
+static Ogl_wrapper ogl = Ogl_wrapper(1000,1000, "tests geometry");
+static bool continue_flag = true;
+
+int main(void)
+{
+    ogl.setup_input_callback((void*)key_callback);
+	ogl.ogl_link_world(&world);
+
+    // square
+	world.add_element(Square(0,0,0.2f,0.2f, Color(0,1,0)));
+	
+	// circle
+	world.add_element(Circle(0.5f,0.5f,0.2f, Color(0,1,1)));
+	
+	while (continue_flag) {	
+		ogl.ogl_calc_vertex_array();
+		ogl.ogl_redraw();
+		usleep(500);
+	}
+	return 0;
+}
+
+
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    	glfwSetWindowShouldClose(window, GL_TRUE);
+		continue_flag = false;
+	}
+}
