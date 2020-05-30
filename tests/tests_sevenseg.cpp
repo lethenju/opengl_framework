f7dc124eaa21ff309186a00f05df00058daa7b8a
Julien LE THENO <julien.letheno@gmail.com>
2020-05-30 09:38:57 +0200

    [Up] Add some letters in the tests

--- tests/tests_sevenseg.cpp
+++ tests/tests_sevenseg.cpp
@@ -17,12 +17,18 @@ int main(void)
     ogl.setup_input_callback((void*)key_callback);
 	ogl.ogl_link_world(&world);
 	bool a[7] = {0, 1, 1, 1, 1, 1, 1};
-	bool b[7] = {1, 1, 1, 1, 1, 1, 1};
-	bool c[7] = {1, 1, 0, 0, 1, 0, 1};
+	bool b[7] = {1, 1, 1, 1, 1, 0, 0};
+	bool c[7] = {1, 1, 0, 1, 0, 0, 0};
+	bool d[7] = {1, 1, 1, 1, 0, 1, 0};
+	bool e[7] = {1, 1, 0, 1, 1, 0, 1};
+	bool f[7] = {0, 1, 0, 1, 1, 0, 1};
     // seven seg
 	int a_id = world.add_element(SevenSegment(a, 0,0,0.1f,0.2f, Color(1,1,1)));
-	int b_id = world.add_element(SevenSegment(b, 0.12f,0,0.1f,0.2f, Color(1,1,1)));
-	int c_id = world.add_element(SevenSegment(c, 0.24f,0,0.1f,0.2f, Color(1,1,1)));
+	int b_id = world.add_element(SevenSegment(b, 0.12f,0,0.1f,0.2f, Color(0.9f,1,1)));
+	int c_id = world.add_element(SevenSegment(c, 0.24f,0,0.1f,0.2f, Color(0.8f,1,1)));
+	int d_id = world.add_element(SevenSegment(d, 0.36f,0,0.1f,0.2f, Color(0.7f,1,1)));
+	int e_id = world.add_element(SevenSegment(e, 0.48f,0,0.1f,0.2f, Color(0.6f,1,1)));
+	int f_id = world.add_element(SevenSegment(f, 0.60f,0,0.1f,0.2f, Color(0.5f,1,1)));
 
 	while (continue_flag) {
 		ogl.ogl_calc_vertex_array();
