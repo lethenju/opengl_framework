76b93dc813924e9867564eb75b09acae24fbd4c4
Julien LE THENO <julien.letheno@gmail.com>
2020-06-15 18:47:44 +0200

    [Add] Skeletton of the text module

---
+++ src/text.hpp
@@ -0,0 +1,43 @@
+/*
+MIT License
+
+Copyright (c) 2020 Julien LE THENO
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#include <vector>
+#include "geom.hpp"
+
+/* A Letter, created from a char, actually a wrapper around SevenSegment
+ * only letters through a-z and numbers are working
+ */
+class Letter {
+    Letter(char c, float x, float y, float sizeX, float sizeY, float thickness, Color color);
+    /** Public Seven Segment representation of the letter, to move it to some place 
+    * when needed
+    */
+    SevenSegment repr;
+};
+
+/** The text is a vector of letters, with additionnal properties as the
+ * space between letters etc.
+ */
+class Text : public std::vector<Letter> {
+
+};
